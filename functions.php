7748fd0cc6487ecfa826cd6c8cf7b04c0a56585c
Veronica <veasleydesign@gmail.com>
2017-01-08 17:46:59 -0500

    social share added

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function add_custom_blog_header(){ ?>
   <div class="custom-header-container">
       
       <!-- our custom header codes here -->
-      <div id="x-section-1" class="x-section _before _overlay-grey inner-page-hero bg-image blog-header" style="margin: 0px 0px 45px;padding: 5.5% 0px; background-image: url(/wp-content/uploads/2016/12/gad-articles2.jpg); background-color: transparent;" data-x-element="section" data-x-params="{&quot;type&quot;:&quot;image&quot;,&quot;parallax&quot;:false}">
+      <div id="x-section-1" class="x-section _before _overlay-grey inner-page-hero bg-image blog-header" style="margin: 0px 0px 45px;padding: 5.3% 0px; background-image: url(/wp-content/uploads/2016/12/gad-articles2.jpg); background-color: transparent;" data-x-element="section" data-x-params="{&quot;type&quot;:&quot;image&quot;,&quot;parallax&quot;:false}">
           <div class="x-container max width" style="margin: 0px auto;padding: 0px;">
               <div class="x-column x-sm x-1-1" style="padding: 0px; opacity: 1; transition-duration: 750ms; transform: translate(0px, 0px);" data-x-element="column" data-x-params="{&quot;fade&quot;:true}" data-fade="true">
                   <h1 class="x-text-type h3" data-x-element="text_type" data-x-params="{&quot;strings&quot;:[&quot;Blog&quot;],&quot;type_speed&quot;:50,&quot;start_delay&quot;:0,&quot;back_speed&quot;:50,&quot;back_delay&quot;:3000,&quot;loop&quot;:false,&quot;show_cursor&quot;:true,&quot;cursor&quot;:&quot;|&quot;}">
@@ -68,7 +68,7 @@ function add_custom_archive_header(){ ?>
 	  <div class="custom-header-container">
 		  
 	      <!-- our custom header codes here -->
-	      <div id="x-section-1" class="x-section _before _overlay-grey inner-page-hero bg-image" style="margin: 0px 0px 45px;padding: 5.5% 0px; background-image: url(/wp-content/uploads/2016/12/gad-articles1.jpg); background-color: transparent;" data-x-element="section" data-x-params="{&quot;type&quot;:&quot;image&quot;,&quot;parallax&quot;:false}">
+	      <div id="x-section-1" class="x-section _before _overlay-grey inner-page-hero bg-image" style="margin: 0px 0px 45px;padding: 5.3% 0px; background-image: url(/wp-content/uploads/2016/12/gad-articles1.jpg); background-color: transparent;" data-x-element="section" data-x-params="{&quot;type&quot;:&quot;image&quot;,&quot;parallax&quot;:false}">
               <div class="x-container max width" style="margin: 0px auto;padding: 0px;">
                   <div class="x-column x-sm x-1-1" style="padding: 0px; opacity: 1; transition-duration: 750ms; transform: translate(0px, 0px);" data-x-element="column" data-x-params="{&quot;fade&quot;:true}" data-fade="true">
                       <h1 class="x-text-type h3" data-x-element="text_type" data-x-params="{&quot;strings&quot;:[&quot;Archives&quot;],&quot;type_speed&quot;:50,&quot;start_delay&quot;:0,&quot;back_speed&quot;:50,&quot;back_delay&quot;:3000,&quot;loop&quot;:false,&quot;show_cursor&quot;:true,&quot;cursor&quot;:&quot;|&quot;}">
@@ -85,3 +85,18 @@ function add_custom_archive_header(){ ?>
 <?php }
 
 add_action('x_before_view_ethos__landmark-header', 'add_custom_archive_header');
+
+
+
+
+
+// Add Social Share icons
+// =============================================================================
+
+add_action('x_before_the_content_end', function() {
+	
+  if ( is_singular( 'post' ) ){
+      echo do_shortcode('[share title="Share this Post" facebook="true" twitter="true" google_plus="true" linkedin="true" pinterest="true" reddit="true" email="true"]');
+  }
+  
+});
